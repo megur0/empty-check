8f85450307bb4448e70344c3fa5cebab058540cd
megur0 <119830503+megur0@users.noreply.github.com>
2025-02-13 12:23:40 +0900

    initial

---
+++ empty_check.go
@@ -0,0 +1,152 @@
+package emptycheck
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/google/uuid"
+)
+
+type RequiredFieldError struct {
+	Field string
+}
+
+func (e RequiredFieldError) Error() string {
+	return fmt.Sprintf("required field %s is empty", e.Field)
+}
+
+// 構造体のフィールドのすべてがemptyでないことをチェックする。
+// タグでrequire:"noRequired"としている場合は、チェックをしない。
+// フィールドはすべてExported fieldである必要がある。
+func EmptyCheck[S any](s *S) error {
+	rv := reflect.ValueOf(s).Elem()
+	if rv.Kind() != reflect.Struct {
+		panic("arg must be pointer to struct")
+	}
+	return emptyCheck(rv, "")
+}
+
+func emptyCheck(rv reflect.Value, parentTagName string) error {
+	if rv.Kind() == reflect.Slice || rv.Kind() == reflect.Array {
+		if rv.Len() == 0 {
+			return RequiredFieldError{
+				Field: parentTagName,
+			}
+		}
+		if _, ok := rv.Interface().(uuid.UUID); ok {
+			if isStructFieldEmpty(rv) {
+				return RequiredFieldError{
+					Field: parentTagName,
+				}
+			}
+			return nil
+		}
+		for i := 0; i < rv.Len(); i++ {
+			if rv.Index(i).Kind() == reflect.Struct {
+				switch rv.Index(i).Interface().(type) {
+				case Zeroable, Emptiable:
+					if isStructFieldEmpty(rv.Field(i)) {
+						return RequiredFieldError{
+							Field: parentTagName,
+						}
+					}
+				default:
+					if err := emptyCheck(rv.Index(i), parentTagName); err != nil {
+						return err
+					}
+				}
+			} else if rv.Index(i).Kind() == reflect.Slice || rv.Index(i).Kind() == reflect.Array {
+				if err := emptyCheck(rv.Index(i), parentTagName); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	}
+
+	if rv.Kind() == reflect.Struct {
+		rt := rv.Type()
+		for i := 0; i < rt.NumField(); i++ {
+			//fmt.Println(rt.Field(i).Name)
+			v := rt.Field(i).Tag.Get("require")
+			if v == "noRequired" {
+				continue
+			}
+			if rv.Field(i).Kind() == reflect.Struct {
+				switch rv.Field(i).Interface().(type) {
+				case Zeroable, Emptiable:
+					if isStructFieldEmpty(rv.Field(i)) {
+						return RequiredFieldError{
+							Field: rt.Field(i).Name,
+						}
+					}
+				default:
+					err := emptyCheck(rv.Field(i), rt.Field(i).Name)
+					if err != nil {
+						return err
+					}
+				}
+			} else if rv.Field(i).Kind() == reflect.Slice || rv.Field(i).Kind() == reflect.Array {
+				err := emptyCheck(rv.Field(i), rt.Field(i).Name)
+				if err != nil {
+					return err
+				}
+			} else {
+				if isStructFieldEmpty(rv.Field(i)) {
+					return RequiredFieldError{
+						Field: rt.Field(i).Name,
+					}
+				}
+			}
+		}
+		return nil
+	}
+
+	panic(fmt.Sprintf("unexpected type: %T", rv.Interface()))
+}
+
+// time.Timeや*time.Time（nilではないもの）が該当する
+type Zeroable interface {
+	IsZero() bool
+}
+
+// Emptiableを実装することで独自にEmptyを定義可能
+type Emptiable interface {
+	IsEmpty() bool
+}
+
+func isStructFieldEmpty(rv reflect.Value) bool {
+	if rv.Kind() == reflect.Ptr {
+		return rv.IsNil()
+	}
+	switch v := rv.Interface().(type) {
+	case string:
+		return v == ""
+	case int:
+		return v == 0
+	case uint:
+		return v == 0
+	case int8:
+		return v == 0
+	case int16:
+		return v == 0
+	case int32:
+		return v == 0
+	case int64:
+		return v == 0
+	case float32:
+		return v == 0
+	case float64:
+		return v == 0
+	case bool:
+		return !v
+	case uuid.UUID:
+		return v == uuid.Nil
+	case Zeroable:
+		return v.IsZero()
+	case Emptiable:
+		return v.IsEmpty()
+	default:
+		panic(fmt.Sprintf("unexpected type: %T", v))
+	}
+}
---
+++ empty_check_test.go
@@ -0,0 +1,231 @@
+package emptycheck
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/megur0/testutil"
+
+	"github.com/google/uuid"
+)
+
+// go test -v -run ^TestEmptyCheck
+
+// go test -v -run ^TestEmptyCheckIsStructFieldEmpty$
+func TestEmptyCheckIsStructFieldEmpty(t *testing.T) {
+	for _, v := range []struct {
+		v      reflect.Value
+		expect bool
+	}{
+		{
+			v:      reflect.ValueOf("test"),
+			expect: false,
+		},
+		{
+			v:      reflect.ValueOf(3),
+			expect: false,
+		},
+		{
+			v:      reflect.ValueOf(Ptr(3)),
+			expect: false,
+		},
+		{
+			v:      reflect.ValueOf(Ptr("test")),
+			expect: false,
+		},
+		{
+			v:      reflect.ValueOf(time.Now()),
+			expect: false,
+		},
+		{
+			v:      reflect.ValueOf(true),
+			expect: false,
+		},
+		{
+			v:      reflect.ValueOf(false),
+			expect: true,
+		},
+		{
+			v:      reflect.ValueOf(time.Time{}),
+			expect: true,
+		},
+		{
+			v:      reflect.ValueOf(&time.Time{}),
+			expect: false,
+		},
+		{
+			v:      reflect.ValueOf((*int)(nil)),
+			expect: true,
+		},
+		{
+			v:      reflect.ValueOf((*time.Time)(nil)),
+			expect: true,
+		},
+	} {
+		t.Run("", func(t *testing.T) {
+			if isStructFieldEmpty(v.v) != v.expect {
+				t.Error("unexpected result")
+			}
+		})
+	}
+}
+
+// go test -v -run ^TestEmptyCheck$
+func TestEmptyCheck(t *testing.T) {
+	testutil.AssertEqual(t, EmptyCheck(Ptr(struct {
+		A *int
+	}{})), RequiredFieldError{
+		Field: "A",
+	})
+	testutil.AssertEqual(t, EmptyCheck(Ptr(struct {
+		A string
+	}{})), RequiredFieldError{
+		Field: "A",
+	})
+	testutil.AssertEqual(t, EmptyCheck(Ptr(struct {
+		A int
+	}{})), RequiredFieldError{
+		Field: "A",
+	})
+	testutil.AssertEqual(t, EmptyCheck(Ptr(struct {
+		A uuid.UUID
+	}{})), RequiredFieldError{
+		Field: "A",
+	})
+	testutil.AssertEqual(t, EmptyCheck(Ptr(struct {
+		A time.Time
+	}{})), RequiredFieldError{
+		Field: "A",
+	})
+	testutil.AssertEqual(t, EmptyCheck(Ptr(struct {
+		A testType
+	}{})).Error(), RequiredFieldError{
+		Field: "A",
+	}.Error())
+	testutil.AssertEqual(t, EmptyCheck(Ptr(struct {
+		A []int
+	}{})), RequiredFieldError{
+		Field: "A",
+	})
+	// 配列は長さが0の場合のみemptyとな。要素のゼロバリューはemptyとはならない。
+	testutil.AssertUnTypedNil(t, EmptyCheck(Ptr(struct {
+		A []int
+	}{
+		A: []int{0, 0, 0},
+	})))
+	testutil.AssertEqual(t, EmptyCheck(Ptr(struct {
+		A []uuid.UUID
+	}{})), RequiredFieldError{
+		Field: "A",
+	})
+
+	testutil.AssertUnTypedNil(t, EmptyCheck(Ptr(struct {
+		A []uuid.UUID
+	}{
+		A: []uuid.UUID{
+			uuid.New(),
+		},
+	})))
+	testutil.AssertEqual(t, EmptyCheck(Ptr(struct {
+		A []struct {
+			B int
+		}
+	}{})), RequiredFieldError{
+		Field: "A",
+	})
+	testutil.AssertEqual(t, EmptyCheck(Ptr(struct {
+		A []struct {
+			B int
+		}
+	}{
+		A: []struct {
+			B int
+		}{
+			{},
+		},
+	})), RequiredFieldError{
+		Field: "B",
+	})
+	testutil.AssertUnTypedNil(t, EmptyCheck(Ptr(struct {
+		A []struct {
+			B int
+		}
+	}{
+		A: []struct {
+			B int
+		}{
+			{
+				B: 1,
+			},
+		},
+	})))
+	testutil.AssertUnTypedNil(t, EmptyCheck(Ptr(struct {
+		A testType `require:"noRequired"`
+	}{})))
+	testutil.AssertEqual(t, EmptyCheck(Ptr(testTypeStruct{
+		Child: testTypeStructChild{
+			Arr: []string{"", ""},
+			Child: testTypeStructChildChild{
+				B: false,
+			},
+			Child2: []testTypeStructChildChild2{
+				{
+					C: true,
+				},
+			},
+		},
+		Num: 1,
+		Str: "dummy",
+	})), RequiredFieldError{
+		Field: "B",
+	})
+
+	testutil.AssertEqual(t, EmptyCheck(Ptr(testTypeStruct{
+		Child: testTypeStructChild{
+			Arr: []string{"", ""},
+			Child: testTypeStructChildChild{
+				B: true,
+			},
+			Child2: []testTypeStructChildChild2{
+				{
+					C: false,
+				},
+			},
+		},
+		Num: 1,
+		Str: "dummy",
+	})), RequiredFieldError{
+		Field: "C",
+	})
+}
+
+type testType string
+
+func (tt testType) IsEmpty() bool {
+	return tt == ""
+}
+
+type testTypeStruct struct {
+	Child testTypeStructChild
+	Num   int
+	Str   string
+}
+
+type testTypeStructChild struct {
+	Arr    []string
+	Child  testTypeStructChildChild
+	Child2 []testTypeStructChildChild2
+}
+
+type testTypeStructChildChild struct {
+	B bool
+}
+
+type testTypeStructChildChild2 struct {
+	C bool
+}
+
+func Ptr[T any](a T) *T {
+	return &a
+}
